8e6f0bd7dc8c01eb2862109bfb85b2cce0ff3560
lvp <lvp@ziroom.com>
2019-01-25 16:23:14 +0800

    Initial commit

---
+++ aibaseui/src/main/java/com/alizhezi/aibaseui/AiBaseActivity.java
@@ -0,0 +1,12 @@
+package com.alizhezi.aibaseui;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AiBaseActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
