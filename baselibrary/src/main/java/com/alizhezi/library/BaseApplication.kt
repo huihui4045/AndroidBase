be210b47ffb3ab193fdb9d1177fbfe2550a064aa
lvp <lvp@ziroom.com>
2019-11-22 21:45:11 +0800

    baseApplication

---
+++ baselibrary/src/main/java/com/alizhezi/library/BaseApplication.kt
@@ -0,0 +1,15 @@
+package com.alizhezi.library
+
+import android.app.Application
+/**
+ * Copyright (C) 2019 The ailizhezi.com Inc. All rights reserved
+ * Author: 吕鹏 gavin6116@gmail.com
+ * Time Created at 2019-11-22 
+ * Description：
+ */
+class BaseApplication:Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
